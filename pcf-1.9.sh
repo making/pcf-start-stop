a25a4df24536fd58c4fb4917949661fdfe47ef20
making <makingx@gmail.com>
2017-03-20 03:14:36 +0900

    Add bosh deployment

--- pcf-1.9.sh
+++ pcf-1.9.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "`which bosh`" == "" ];then
-	alias bosh='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh'
+    alias bosh='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh'
 fi
 
 STOP_SEQ="mysql $STOP_SEQ"
@@ -28,6 +28,10 @@ START_SEQ=""
 for i in $STOP_SEQ;do
     START_SEQ="$i $START_SEQ"
 done
+
+if [ -d /var/tempest/workspaces/default/deployments ];then
+    bosh deployment `ls /var/tempest/workspaces/default/deployments/cf*`
+fi
 
 case $1 in
     start)
