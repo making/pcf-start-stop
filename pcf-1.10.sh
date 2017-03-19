492757d9014184b4b317769a24950d4e0e719362
making <makingx@gmail.com>
2017-03-20 05:06:31 +0900

    Change orders

--- pcf-1.10.sh
+++ pcf-1.10.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 alias bosh='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh'
 
-STOP_SEQ="mysql $STOP_SEQ"
 STOP_SEQ="consul_server $STOP_SEQ"
 STOP_SEQ="nats $STOP_SEQ"
 STOP_SEQ="etcd_tls_server $STOP_SEQ"
 # STOP_SEQ="nfs_server $STOP_SEQ"
-STOP_SEQ="router $STOP_SEQ"
 STOP_SEQ="mysql_proxy $STOP_SEQ"
+STOP_SEQ="mysql $STOP_SEQ"
+STOP_SEQ="diego_database $STOP_SEQ"
 STOP_SEQ="uaa $STOP_SEQ"
 STOP_SEQ="cloud_controller $STOP_SEQ"
+STOP_SEQ="router $STOP_SEQ"
 # STOP_SEQ="ha_proxy $STOP_SEQ"
 STOP_SEQ="clock_global $STOP_SEQ"
 STOP_SEQ="cloud_controller_worker $STOP_SEQ"
-STOP_SEQ="tcp_router $STOP_SEQ"
-STOP_SEQ="diego_database $STOP_SEQ"
+STOP_SEQ="doppler $STOP_SEQ"
 STOP_SEQ="diego_brain $STOP_SEQ"
+STOP_SEQ="loggregator_trafficcontroller $STOP_SEQ"
 STOP_SEQ="diego_cell $STOP_SEQ"
 STOP_SEQ="mysql_monitor $STOP_SEQ"
-STOP_SEQ="doppler $STOP_SEQ"
-STOP_SEQ="loggregator_trafficcontroller $STOP_SEQ"
+STOP_SEQ="tcp_router $STOP_SEQ"
 
 START_SEQ=""
 for i in $STOP_SEQ;do
