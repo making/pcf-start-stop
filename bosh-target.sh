b1bf26bd05be61dba92a3a964a250b5c94102656
making <makingx@gmail.com>
2017-03-21 00:45:15 +0900

    update

--- bosh-target.sh
+++ bosh-target.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 alias bosh='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh'
 
 if [ "x`which expect`" == "x" ];then
@@ -9,10 +10,12 @@ env
 
 expect -c "
 set timeout 5
-spawn bosh --ca-cert /var/tempest/workspaces/default/root_ca_certificate target ${BOSH_DIRECTOR_IP}
+spawn BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh --ca-cert /var/tempest/workspaces/default/root_ca_certificate target ${BOSH_DIRECTOR_IP}
 expect \"Email:\"
 send \"${BOSH_USER}\n\"
 expect \"Password:\"
 send \"${BOSH_PASSWORD}\n\"
 exit 0
 "
+
+bosh target
